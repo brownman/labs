201d83c6ca2da87480c7a2df7474d0cbe56b683a
bsstoner <bsstoner@gmail.com>
2010-04-28 23:23:04 -0400

    handler fix

--- fbg/api/urls.py
+++ fbg/api/urls.py
@@ -3,7 +3,7 @@ from piston.resource import Resource
 
 from fbg.api.handlers import ScoreHandler
 
-score = Resource(handler=ScoreHandler)
+score = Resource(ScoreHandler)
 
 urlpatterns = patterns('',
     (r'^score/$', score),
