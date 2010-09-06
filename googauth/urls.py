0e582c5cbd95f2dc72f3ef3cbc37c1f3ddbc739e
Brian Stoner <bsstoner@Brian-Stoners-MacBook-Pro.local>
2010-09-06 17:48:33 -0400

    cleaning up old stuff

--- googauth/urls.py
+++
@@ -1,9 +0,0 @@
-from django.conf.urls.defaults import *
-from googauth.views import goog_login, goog_logout, \
-    goog_authenticated
-
-urlpatterns = patterns('',
-    url(r'^login/?$', goog_login),
-    url(r'^logout/?$', goog_logout),
-    url(r'^login/authenticated/?$', goog_authenticated),
-)
