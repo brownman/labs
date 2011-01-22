d46713fda56a7c172f0b94c623a904cf451a6de4
Brian Stoner <bsstoner@Brian-Stoners-MacBook-Pro.local>
2011-01-22 05:40:54 -0500

    added slider

--- slider/images/car2_files/motors_getparams.js
+++
@@ -1,8 +0,0 @@
-//<!--
-//1@@m8
-
-function ReadURL(url){var Inputs=decodeURI(url.search).split('?');var pairs=decodeURI(Inputs[1]).split('&');return(pairs);}
-function getParams(array,variable){var value="";if(!array)
-return(value);var nameVal=array.split('=');if(nameVal[0]==variable){value=nameVal[1];}else{value="";}
-return(value);}
-// b=12593180 -->
